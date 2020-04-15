176dd1f7c385f8e1c11ab6f1a1bddc91ccd5546a
Ezequiel Enano <zekikpoin@gmail.com>
2020-04-15 14:51:48 -0300

    Pide y Muestra los chats

--- src/components/ListChats.js
+++ src/components/ListChats.js
@@ -1,8 +1,22 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 const ListChats = () => {
+    const [chats, setChats] = useState([]);
+    const pedirChats = () => {
+        axios.get('http://localhost:4000/api/chats')
+            .then(response => {
+                setChats(response.data);
+            })
+    }
+    useEffect(() => {
+        pedirChats();
+    })
     return (
         <div>
+            {chats.map(chat =>
+                <div key={chat._id}>
+                    <a href={`http://localhost:4000/api/mensajes/${chat._id}`}>CHAT</a>
+                </div>)}
             <h1>HOLA</h1>
         </div>
     );
