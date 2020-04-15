625fac10aa3bd57ab472323a6bca71ee4bf10ca1
Ezequiel Enano <zekikpoin@gmail.com>
2020-04-15 18:06:23 -0300

    Agrege nuevas funcionalidades al chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,56 +2,19 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
 export default function Chat(props) {
-    const [texto, setTexto] = useState("");
-    const [mensajes, setMensaje] = useState([]);
-
-
-    const pedirMensajes = () => {
-        axios.get('http://localhost:4000/api/mensajes/juan')
-            .then(res => {
-                setMensaje(res.data.mensajes);
-            });
-    }
-
-    useEffect(() => {
-        pedirMensajes();
-
-    })
-
-    const enviarMensaje = (e) => {
-        e.preventDefault()
-        axios({
-            method: "post",
-            url: "http://localhost:4000/api/mensajes",
-            data: {
-                de: "juan",
-                para: "ezequiel",
-                texto: texto,
-            }
+    const [mensajes,setMensajes] = useState([]);
+    const {idChat} = props;
+    const pedirMensajes = ()=>{
+        /* Pide todos los mensajes del chat*/
+        axios.get(`http://localhost:4000/api/mensajes/${idChat}`)
+        .then(response=>{
+            //Guarda los mensajes en el estado
+            setMensajes(response.data);
         })
     }
-    const handleChange = (e) => {
-        const { value } = e.target
-        setTexto(value)
-    }
-    //document.getElementById('final').scrollIntoView(true)
     return (
         <>
-            <div className='messagesBox'>
-                {mensajes.map(msg =>
-                
-                    <div className={msg.de == "ezequiel"? "yo":"elotro" } key={msg._id}>
-                        <label>Mensaje: {msg.texto}</label><br />
-                        <label>Fecha: {msg.createdAt}</label><br />
-                    ==========================<br />
-                    </div>)
-                }
-            </div>
-            <form onSubmit={enviarMensaje}>
-                <input type='text' onChange={handleChange} name='texto' />
-                <button type='submit'>Enviar</button>
-            </form>
-
+            <h1>hola</h1>
         </>
     );
 
